ac3b27e59b82003dcc19b2273dcd3d29f883d70c
sivakit <42512870+sivakit@users.noreply.github.com>
2021-03-28 10:05:07 +0530

    Update build_dev_jar.bat

--- build_dev_jar.bat
+++ build_dev_jar.bat
@@ -1,3 +1,6 @@
 set IA_AppSvr=%~dp0
 
 set JAVA_HOME=%IA_AppSvr%\Tools\jdk1.8.0_152_x64
+
+call %IA_AppSvr%\Tools\apache-ant-1.9.15\bin\ant.bat clean
+call %IA_AppSvr%\Tools\apache-ant-1.9.15\bin\ant.bat build
